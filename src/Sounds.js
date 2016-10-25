85ea4bfab86761372056a698300c37cb6c8b6129
Dalton Menezes <daltonmenezes@outlook.com>
2016-10-25 11:59:19 -0200

    move sounds object to Sounds.js file

---
+++ src/Sounds.js
@@ -0,0 +1,9 @@
+sounds = {
+  music: new Audio("src/sounds/music.mp3"),
+  score: new Audio("src/sounds/score.wav"),
+  jump: new Audio("src/sounds/jump.wav"),
+  collision: new Audio("src/sounds/collision.mp3"),
+  fall: new Audio("src/sounds/fall.wav"),
+  scream: new Audio("src/sounds/chicken-scream.mp3"),
+  start: new Audio("src/sounds/start.mp3")
+}
