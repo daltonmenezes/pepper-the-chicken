f167386ea919a83cdb4bbb4b471711d638fa3768
Dalton Menezes <daltonmenezes@outlook.com>
2016-10-25 20:32:45 -0200

    add groundContext

--- src/Main.js
+++ src/Main.js
@@ -10,9 +10,19 @@ function main() {
   canvas = document.createElement("canvas");
   canvas.width = WIDTH;
   canvas.height = HEIGHT;
-
+  canvas.id = "main";
+  canvas.style.zIndex = "1";
   context = canvas.getContext("2d");
   document.body.appendChild(canvas);
+
+  canvas2 = document.createElement("canvas");
+  canvas2.width = WIDTH;
+  canvas2.height = HEIGHT;
+  canvas2.id = "ground";
+  canvas2.style.zIndex = "-2";
+  groundContext = canvas2.getContext("2d");
+  document.body.appendChild(canvas2);
+
   document.addEventListener("mousedown", click);
   document.addEventListener("keyup", keyUp);
   document.addEventListener("touchstart", touch, false);
