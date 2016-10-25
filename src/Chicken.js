cc1fc1a34ee2fb4dbfe08c755428166fe37ca96e
Dalton Menezes <daltonmenezes@outlook.com>
2016-10-25 11:55:09 -0200

    move block object to Chicken.js file

---
+++ src/Chicken.js
@@ -0,0 +1,85 @@
+block = {
+  x: 50,
+  y: 0,
+  height: chicken[0].height,
+  width: chicken[0].width,
+  gravity: 1.6,
+  speed: 0,
+  jumpForce: 23.6,
+  numberJumps: 0,
+  score: 0,
+  rotation: 0,
+  anim: 0,
+  count: 0,
+  timeChange: 0,
+
+  changeImage: function() {
+    if (this.count <= 17) {
+        if (this.count > 0) {
+            this.anim = 0;
+        }
+        else {
+            this.anim = 1;
+        }
+        this.count++;
+    }
+    else {
+        this.anim = 0;
+        this.count = 1;
+    }
+    this.timeChange = 4;
+  },
+
+  collided: function() {
+    this.count = 18;
+    this.anim = 2;
+  },
+
+  update: function() {
+    this.speed += this.gravity;
+    this.y += this.speed;
+    this.rotation = Math.random() / 90 * speed;
+
+    if (this.y > ground.y - this.height && currentState != states.lost) {
+        this.y = ground.y - this.height;
+        this.numberJumps = 0;
+        this.speed = 0;
+        this.anim = 1;
+    }
+
+    if (this.timeChange == 0) {
+        this.changeImage();
+    }
+    else {
+        this.timeChange--;
+    }
+  },
+
+  jump: function() {
+    if (this.numberJumps < maxJump) {
+        this.speed = -this.jumpForce;
+        this.numberJumps++;
+    }
+  },
+
+  reset: function() {
+    this.speed = 0;
+    this.y = 0;
+    this.anim = 0;
+    this.timeChange = 0;
+
+    if (this.score > rank) {
+        localStorage.setItem("rank", this.score);
+        rank = this.score;
+    }
+    this.score = 0;
+  },
+
+  draw: function() {
+    context.save();
+    context.translate(this.x + this.width / 2, this.y + this.height / 2);
+    context.rotate(this.rotation);
+    chicken[this.anim].draw(-this.width /2, -this.height /2);
+    context.restore();
+  }
+}
