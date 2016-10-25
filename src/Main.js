054db82fb442fafab096808677974fbc6f22da3f
Dalton Menezes <daltonmenezes@outlook.com>
2016-10-25 11:35:24 -0200

    move main function to Main.js file

---
+++ src/Main.js
@@ -0,0 +1,30 @@
+function main() {
+  HEIGHT = window.innerHeight;
+  WIDTH = window.innerWidth;
+
+  if (WIDTH >= 600) {
+      WIDTH = 600;
+      HEIGHT = 550;
+  }
+
+  canvas = document.createElement("canvas");
+  canvas.width = WIDTH;
+  canvas.height = HEIGHT;
+  canvas.style.border = "1px solid #000";
+
+  context = canvas.getContext("2d");
+  document.body.appendChild(canvas);
+  document.addEventListener("mousedown", click);
+  document.addEventListener("keyup", keyUp);
+  document.addEventListener("touchstart", touch, false);
+
+  currentState = states.start;
+  rank = localStorage.getItem("rank");
+
+  if (rank == null) {
+      rank = 0;
+  }
+  sounds.music.loop = true;
+  sounds.music.play();
+  run();
+}
