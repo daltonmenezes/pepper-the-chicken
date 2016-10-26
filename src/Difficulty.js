a2d516016262341f5a15b135abd321690f38e68d
Dalton Menezes <daltonmenezes@outlook.com>
2016-10-26 19:18:29 -0200

    add Difficulty.js

---
+++ src/Difficulty.js
@@ -0,0 +1,13 @@
+function difficulty() {
+  if (chicken.score >= 20)
+      speed = 12;
+
+  if (chicken.score >= 40)
+      speed = 16;
+
+  if (chicken.score >= 80)
+      speed = 19;
+
+  if (chicken.score >= 100)
+      speed = 24;
+}
