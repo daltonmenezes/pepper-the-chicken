3889c52b7acf0e07028094eae5d60feb7ac0b917
Dalton Menezes <daltonmenezes@outlook.com>
2016-10-27 20:22:57 -0200

    preload colliders and chicken sprites

--- src/Draw.js
+++ src/Draw.js
@@ -1,4 +1,15 @@
 function draw() {
+  collider[0].draw(context, 0, 0);
+  collider[1].draw(context, 0, 0);
+  collider[2].draw(context, 0, 0);
+  collider[3].draw(context, 0, 0);
+  collider[4].draw(context, 0, 0);
+  collider[5].draw(context, 0, 0);
+
+  chicken_sprite[0].draw(context, 0, 0);
+  chicken_sprite[1].draw(context, 0, 0);
+  chicken_sprite[2].draw(context, 0, 0);
+    
   background.draw();
 
   if (currentState == states.start) {
