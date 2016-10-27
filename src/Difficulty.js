ef471824eab24ea25883a45b7fba3951bfa8ec52
Dalton Menezes <daltonmenezes@outlook.com>
2016-10-27 00:08:43 -0200

    update the logic to change difficulty and play oh-oh sound when its change

--- src/Difficulty.js
+++ src/Difficulty.js
@@ -1,13 +1,21 @@
 function difficulty() {
-  if (chicken.score >= 20)
+  if (speed == 8 && chicken.score == 20) {
       speed = 12;
+      sounds.difficulty.play();
+  }
 
-  if (chicken.score >= 40)
+  if (speed == 12 && chicken.score == 40) {
       speed = 16;
+      sounds.difficulty.play();
+  }
 
-  if (chicken.score >= 80)
+  if (speed == 16 && chicken.score == 80) {
       speed = 19;
+      sounds.difficulty.play();
+  }
 
-  if (chicken.score >= 100)
+  if (speed == 19 && chicken.score == 100) {
       speed = 24;
+      sounds.difficulty.play();
+  }
 }
