f627a754e91b56976703c166dcff3a66fbe8e2cb
Dalton Menezes <daltonmenezes@outlook.com>
2016-10-30 03:19:02 -0200

    update the size of the sprites to their real sizes. This fix the issue on safari browser

--- src/Sprite.js
+++ src/Sprite.js
@@ -12,8 +12,8 @@ function Sprite(x, y, width, height, path) {
   }
 }
 
-let background_sprite = new Sprite(0, 0, 667, 550, "src/sprites/background.png"), chicken_sprite = [], collider = []
-          menu_sprite = new Sprite(0, 0, 601, 550, "src/sprites/main-menu.png"),
+let background_sprite = new Sprite(0, 0, 667, 500, "src/sprites/background.png"), chicken_sprite = [], collider = [],
+          menu_sprite = new Sprite(0, 0, 601, 498, "src/sprites/main-menu.png"),
           sand = new Sprite(0, 0, 600, 128, "src/sprites/ground.png");
 
 collider[0] = new Sprite(0, 0, 108, 111, "src/sprites/cactus.png");
@@ -23,6 +23,6 @@ collider[3] = new Sprite(0, 0, 124, 73, "src/sprites/stone.png");
 collider[4] = new Sprite(0, 0, 131, 74, "src/sprites/bush.png");
 collider[5] = new Sprite(0, 0, 145, 88, "src/sprites/tree.png");
 
-chicken_sprite[0] = new Sprite(0, 0, 60, 60, "src/sprites/chicken.png");
-chicken_sprite[1] = new Sprite(0, 0, 60, 60, "src/sprites/chicken2.png");
-chicken_sprite[2] = new Sprite(0, 0, 60, 60, "src/sprites/chicken-hit.png");
+chicken_sprite[0] = new Sprite(0, 0, 60, 47, "src/sprites/chicken.png");
+chicken_sprite[1] = new Sprite(0, 0, 60, 47, "src/sprites/chicken2.png");
+chicken_sprite[2] = new Sprite(0, 0, 60, 47, "src/sprites/chicken-hit.png");
