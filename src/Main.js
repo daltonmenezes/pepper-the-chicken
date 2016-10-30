22f92fece6949d69d7f17ad32ca5250f62e3ca56
Dalton Menezes <daltonmenezes@outlook.com>
2016-10-30 01:59:14 -0200

    add a third canvas to receive the animated background

--- src/Main.js
+++ src/Main.js
@@ -8,7 +8,7 @@ function main() {
   canvas.width = WIDTH;
   canvas.height = HEIGHT;
   canvas.id = "main";
-  canvas.style.zIndex = "1";
+  canvas.style.zIndex = "3";
   context = canvas.getContext("2d");
   document.body.appendChild(canvas);
 
@@ -16,9 +16,17 @@ function main() {
   canvas2.width = WIDTH;
   canvas2.height = HEIGHT;
   canvas2.id = "ground";
-  canvas2.style.zIndex = "-2";
+  canvas2.style.zIndex = "2";
   groundContext = canvas2.getContext("2d");
   document.body.appendChild(canvas2);
+
+  canvas3 = document.createElement("canvas");
+  canvas3.width = WIDTH;
+  canvas3.height = HEIGHT;
+  canvas3.id = "background";
+  canvas3.style.zIndex = "1";
+  backgroundContext = canvas3.getContext("2d");
+  document.body.appendChild(canvas3);
 
   document.addEventListener("mousedown", click);
   document.addEventListener("keydown", keyDown);
