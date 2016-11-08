d48604643ddf0d39bd5014e4b75e313ea2c3eada
Dalton Menezes <daltonmenezes@outlook.com>
2016-11-08 11:12:06 -0200

    Delegate the canvas creation to CanvasCreator object

--- src/Main.js
+++ src/Main.js
@@ -1,32 +1,15 @@
 function main() {
-  if (WIDTH >= 600) {
-      WIDTH = 600;
-      HEIGHT = 550;
-  }
+  context = canvasCreator;
+  context.id = 'main';
+  context = context.create();
 
-  canvas = document.createElement("canvas");
-  canvas.width = WIDTH;
-  canvas.height = HEIGHT;
-  canvas.id = "main";
-  canvas.style.zIndex = "3";
-  context = canvas.getContext("2d");
-  document.body.appendChild(canvas);
+  groundContext = canvasCreator;
+  groundContext.id = 'ground';
+  groundContext = groundContext.create();
 
-  canvas2 = document.createElement("canvas");
-  canvas2.width = WIDTH;
-  canvas2.height = HEIGHT;
-  canvas2.id = "ground";
-  canvas2.style.zIndex = "2";
-  groundContext = canvas2.getContext("2d");
-  document.body.appendChild(canvas2);
-
-  canvas3 = document.createElement("canvas");
-  canvas3.width = WIDTH;
-  canvas3.height = HEIGHT;
-  canvas3.id = "background";
-  canvas3.style.zIndex = "1";
-  backgroundContext = canvas3.getContext("2d");
-  document.body.appendChild(canvas3);
+  backgroundContext = canvasCreator;
+  backgroundContext.id = 'background';
+  backgroundContext = backgroundContext.create();
 
   document.addEventListener("mousedown", click);
   document.addEventListener("keydown", keyDown);
