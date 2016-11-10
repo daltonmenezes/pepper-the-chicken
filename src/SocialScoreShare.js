5e0f754bb839b8e5ea8bc858b457f591f28fa7a0
Dalton Menezes <daltonmenezes@outlook.com>
2016-11-10 20:02:45 -0200

    Update the title and description to share the score on Facebook

--- src/SocialScoreShare.js
+++ src/SocialScoreShare.js
@@ -3,8 +3,8 @@ function socialScoreShare() {
   let top = (screen.height/2)-(450/2);
 
   caption = 'Pepper - The Chicken';
-  title = 'Pepper - The Chicken!';
-  description = 'I just scored '+chicken.score+' on Pepper - The Chicken. Can you beat it?';
+  title = chicken.score+' points!!!';
+  description = 'I just scored '+chicken.score+' points on Pepper - The Chicken. Can you beat it? #pepperthechicken';
 
   window = window.open ('https://www.facebook.com/dialog/share?app_id=998325273610775&href=https://daltonmenezes.github.io/pepper-the-chicken/&picture=https://daltonmenezes.github.io/pepper-the-chicken/src/sprites/pepper-fb.png&title='+title+'&caption='+caption+'&description='+description, '_blank', 'width=250, height=250, top='+ top +', left='+ left);
 }
